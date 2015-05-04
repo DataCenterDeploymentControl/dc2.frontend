f2555b90eea707025bef53d7cf7b8efcbba09e55
Stephan Adig <sh@sourcecode.de>
2015-05-05 00:38:10 +0200

    fixed

--- app/js/app.js
+++ app/js/app.js
@@ -131,11 +131,11 @@ dc2Factories.factory('IPNetworkFactory', ['$resource', IPNetworkFactory]);
 
 
 function IPNetworkInfoFactory($resource) {
-  return $resource("http://localhost:5000/api/ipam/v1/ipnetworks/info/:ipnetwork", {ipnetwork:null}, {
+  return $resource("http://localhost:5000/api/ipam/v1/ipnetworks/info", {ipnetwork:null}, {
     get: {
       method:'GET',
       isArray:false,
-      params:{ipnetwork:"@ipnetwork"}
+      data:{ipnetwork:"@ipnetwork"}
     } 
   })
 }
